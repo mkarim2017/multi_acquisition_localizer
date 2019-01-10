39ee63160cbba12c071f83d2ce4f11d4f2763668
Mohammed Karim <mkarom@jpl.nasa.gov>
2019-01-10 02:47:32 +0000

    single_acquisition_localizer

---
+++ single_acquisition_localizer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import os, sys, time, json, requests, logging
+import acquisition_localizer_single
+
+def main():
+
+    
+    context_file = os.path.abspath("_context.json")
+    if not os.path.exists(context_file):
+        raise(RuntimeError("Context file doesn't exist."))
+    #sling_acquisitions.resolve_source(context_file)    
+    acquisition_localizer_single.resolve_source_from_ctx(context_file)
+if __name__ == "__main__":
+    sys.exit(main())
